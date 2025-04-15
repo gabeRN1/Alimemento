15fb7a0348c54f520526098e3f2b147f1488e608
Gabriel Reis <gabj111222@gmail.com>
2025-04-15 09:11:11 -0300

    atualização para deploy0.2

--- app/api/meals/route.ts
+++ app/api/meals/route.ts
@@ -63,37 +63,3 @@ export async function GET(req: Request) {
     );
   }
 }
-export async function DELETE(req: Request, { params }: { params: { id: string } }) {
-  try {
-    const { id } = params;
-
-    // Verifica se o id é um ObjectId válido
-    if (!ObjectId.isValid(id)) {
-      return new Response(
-        JSON.stringify({ error: "ID inválido" }),
-        { status: 400, headers: { "Content-Type": "application/json" } }
-      );
-    }
-
-    const client = await clientPromise;
-    const db = client.db("ProjectAlimentation");
-
-    // Deleta a refeição com o ObjectId correspondente
-    const result = await db.collection("meals").deleteOne({ _id: new ObjectId(id) });
-
-    if (result.deletedCount === 0) {
-      return new Response(
-        JSON.stringify({ error: "Refeição não encontrada" }),
-        { status: 404, headers: { "Content-Type": "application/json" } }
-      );
-    }
-
-    return NextResponse.json({ message: "Refeição excluída com sucesso" });
-  } catch (error) {
-    console.error("Erro ao excluir refeição:", error);
-    return new Response(
-      JSON.stringify({ error: "Erro ao excluir refeição" }),
-      { status: 500, headers: { "Content-Type": "application/json" } }
-    );
-  }
-}
